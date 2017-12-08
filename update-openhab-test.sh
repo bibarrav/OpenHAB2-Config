e645d56d115e41249a07db94f56de8a64c3bc41b
bibarrav <bibarrav@hotmail.com>
2017-12-07 23:33:48 -0300

    Updated test env script

--- update-openhab-test.sh
+++ update-openhab-test.sh
@@ -2,7 +2,7 @@
                                                                                                             
                                                                                                             
 #Update docker image, stop and  destroy existing container                                                  
-docker pull openhab/openhab:2.2.0-snapshot-amd64                                                            
+#docker pull openhab/openhab:2.2.0-snapshot-amd64                                                            
 docker stop openhab-test                                                                                    
 docker rm openhab-test                                                                                      
                                                                                                             
@@ -10,8 +10,8 @@ docker rm openhab-test
 rm -rf /opt/openhab-test/userdata/cache/*                                                                   
 rm -rf /opt/openhab-test/userdata/tmp/*                                                                     
 rm -rf /opt/openhab/userdata/tmp/.*                                                                         
-rm /opt/openhab/userdata/karaf.pid                                                                          
-rm /opt/openhab/userdata/lock                                                                               
+rm /opt/openhab-test/userdata/karaf.pid                                                                          
+rm /opt/openhab-test/userdata/lock                                                                               
 chown -R openhab:openhab /opt/openhab-test                                                                  
                                                                                                             
 #Re-Create Openhab container using the new image                                                            
